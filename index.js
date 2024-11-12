5cb87b122e06aeea1f00634cea6d4bdbd1b92613
Danwidj <daniel.widjaja18@gmail.com>
2024-11-12 15:45:54 +0800

    twat

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const PORT = 4000;
 app.get('/home', (req, res) => {
     res.status(200).json('Welcome, your app is working well');
 });
+app.get('/', (req, res) => {
+    res.status(200).json('TESTING');
+});
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
