1637354548a1ac7cd12d8356d920431ed443ca5e
Danwidj <daniel.widjaja18@gmail.com>
2024-11-12 13:48:50 +0800

    new backend

--- index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const express = require('express');
-const app = express();
-const PORT = 4000
-
-app.get('/',(req,res) => {
-    res.status(200).json('Hey there i am ready')
-})
