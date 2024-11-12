2b0d06318cf3a2d16a4639b82c03603cdd13642f
Danwidj <daniel.widjaja18@gmail.com>
2024-11-12 15:48:34 +0800

    testing

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import cors from 'cors';
+import axios from 'axios';
+import stripePackage from 'stripe';
+import bodyParser from 'body-parser';
+import path from 'path';
+import nodemailer from 'nodemailer';
+
+
+
 const express = require('express');
 const app = express();
 const PORT = 4000;
+const stripe = stripePackage(process.env.STRIPE_SECRET_KEY);
+
 
 app.get('/home', (req, res) => {
     res.status(200).json('Welcome, your app is working well');
 });
 app.get('/', (req, res) => {
-    res.status(200).json('TESTING');
+    res.status(200).json(stripe);
 });
+
+
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
