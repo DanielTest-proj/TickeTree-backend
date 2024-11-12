cd2d3031d1b21274831cfb437f4737817346fe7e
Danwidj <daniel.widjaja18@gmail.com>
2024-11-12 15:42:02 +0800

    new

--- vercel-app/index.js
+++ vercel-app/index.js
@@ -1,16 +1,13 @@
-import express from 'express'
-const app = express()
-const PORT = 4000
-
+const express = require('express');
+const app = express();
+const PORT = 4000;
 
 app.get('/home', (req, res) => {
     res.status(200).json('Welcome, your app is working well');
-})
-
+});
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
 
-// Export the Express API
-module.exports = app
+module.exports = app;
